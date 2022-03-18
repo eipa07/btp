cce863b65f184e09b9e45c0024726c0f89c05812
EIPA <erick.pina007@gmail.com>
2022-03-18 06:12:07 +0000

    inicio

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("ns3.project1.controller.View1", {
+            onInit: function () {
+
+                // Commentf
+            }
+        });
+    });
